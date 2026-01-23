ec38805f656e55c495f7780e1e765f61b2b3a06f
nkermani <kermani.nathan@gmail.com>
2026-01-23 11:04:20 +0100

    feat:added lsp files might need more

--- dot_config/nvim/lsp/lsp.lua
+++ dot_config/nvim/lsp/lsp.lua
@@ -2,3 +2,9 @@
 -- Main place to run LSP
 
 vim.lsp.enable({ "lua_ls" })
+vim.lsp.enable({ "pyright" })
+vim.lsp.enable({ "ts_ls" })
+vim.lsp.enable({ "bashls" })
+vim.lsp.enable({ "dotls" })
+vim.lsp.enable({ "clangd" })
+vim.lsp.enable({ "omnisharp" })
