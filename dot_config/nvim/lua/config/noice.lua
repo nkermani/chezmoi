faabf15f3d85e2dd4ae9f1db6f2b137694e9e2be
nkermani <kermani.nathan@gmail.com>
2026-01-30 14:02:49 +0100

    update: disabled noice messages on save

--- dot_config/nvim/lua/config/noice.lua
+++ dot_config/nvim/lua/config/noice.lua
@@ -1,6 +1,21 @@
 -- lua/config/noice.lua
 -- folke/noice.nvim -> Highly experimental plugin that completely replaces the UI for messages, cmdline and the popupmenu
 require("noice").setup({
-    lsp = { override = { ["vim.lsp.util.convert_input_to_markdown_lines"] = true } },
+    lsp = { 
+        override = { ["vim.lsp.util.convert_input_to_markdown_lines"] = true },
+        -- Désactive le message "LSP Progress" qui spamme lors de la sauvegarde
+        progress = { enabled = false },
+    },
     presets = { bottom_search = true, command_palette = true, long_message_to_split = true },
+    -- Filtrer les messages de notification spammy
+    routes = {
+        {
+            filter = {
+                event = "msg_show",
+                kind = "",
+                find = "written",
+            },
+            opts = { skip = true },
+        },
+    },
 })
--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -50,6 +50,8 @@ opt.ignorecase = true -- Ignore la casse lors de la recherche...
 opt.smartcase = true  -- ...sauf s'il y a une majuscule
 opt.updatetime = 250  -- Plus rapide pour les diagnostics LSP
 opt.scrolloff = 8     -- Garde 8 lignes visibles en haut/bas
+opt.backspace = "indent,eol,start" -- Permet à backspace de supprimer : indentation, retours à la ligne (eol) et avant l'insertion (start)
+
 
 -- Synchronise le presse-papier de Neovim avec le presse-papier du système
 -- Cela permet de copier dans Neovim et coller dans Chrome (et inversement) sans config spéciale
