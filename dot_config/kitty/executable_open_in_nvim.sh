219186f893b52c79782de2acda505ebf212231f5
nkermani <kermani.nathan@gmail.com>
2026-02-02 00:41:35 +0100

    Better hints

--- dot_config/kitty/executable_open_in_nvim.sh
+++ dot_config/kitty/executable_open_in_nvim.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-kitty @ launch --type=overlay /Users/nkermani/.nkermani/bin/nvim "$@"
+input="$1"
+
+if [[ "$input" =~ ^(.+):([0-9]+):([0-9]+) ]]; then
+    file="${BASH_REMATCH[1]}"
+    line="${BASH_REMATCH[2]}"
+    col="${BASH_REMATCH[3]}"
+    kitty @ launch --type=overlay /Users/nkermani/.nkermani/bin/nvim "+call cursor($line,$col)" "$file"
+elif [[ "$input" =~ ^(.+):([0-9]+) ]]; then
+    file="${BASH_REMATCH[1]}"
+    line="${BASH_REMATCH[2]}"
+    kitty @ launch --type=overlay /Users/nkermani/.nkermani/bin/nvim "+$line" "$file"
+else
+    kitty @ launch --type=overlay /Users/nkermani/.nkermani/bin/nvim "$input"
+fi
