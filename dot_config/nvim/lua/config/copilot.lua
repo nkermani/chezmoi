772df72fb221d4647e2d1f26ef9a07ad432f0555
nkermani <kermani.nathan@gmail.com>
2026-02-03 15:45:06 +0100

    feat: add new configurations for Copilot, OpenCode agent, theme, and environment variables

--- dot_config/nvim/lua/config/copilot.lua
+++ dot_config/nvim/lua/config/copilot.lua
@@ -1,8 +1,8 @@
--- lua/config/copilot.lua
 local status_ok, copilot = pcall(require, "copilot")
 if not status_ok then return end
 
 copilot.setup({
+    copilot_node_command = vim.fn.expand("$HOME/.nkermani/apps/nvm/versions/node/v22.22.0/bin/node"),
     suggestion = {
         enabled = true,
         auto_trigger = true,
