bd8f89afd3a6e302a847498d0dff987bfa8a9817
nkermani <kermani.nathan@gmail.com>
2026-02-11 12:59:02 +0100

    feat(oil): add directory change functionality with Telescope integration

--- dot_config/nvim/lua/config/oil.lua
+++ dot_config/nvim/lua/config/oil.lua
@@ -64,5 +64,78 @@ require("oil").setup({
     },
 })
 
+vim.api.nvim_create_autocmd("BufEnter", {
+    group = vim.api.nvim_create_augroup("OilAutoCd", { clear = true }),
+    pattern = "oil://*",
+    callback = function()
+        local dir = require("oil").get_current_dir()
+        if dir then
+            pcall(vim.api.nvim_set_current_dir, dir)
+        end
+    end,
+})
+
+local function change_directory(opts)
+    opts = opts or {}
+    local open_oil = opts.open_oil or false
+    local search_cwd = opts.cwd or os.getenv("HOME")
+    local prompt = opts.prompt_title or (open_oil and "Change Directory & Open Oil" or "Change Directory")
+
+    require('telescope.builtin').find_files({
+        prompt_title = prompt,
+        cwd = search_cwd,
+        find_command = { "fd", "--type", "d", "--hidden", "--exclude", ".git", "--exclude", "Library" },
+        previewer = false,
+        attach_mappings = function(prompt_bufnr, map)
+            local actions = require('telescope.actions')
+            local action_state = require('telescope.actions.state')
+
+            map('i', '<CR>', function()
+                local selection = action_state.get_selected_entry()
+                actions.close(prompt_bufnr)
+                if selection then
+                    local path = selection.path or selection[1]
+                    local full_path = path
+                    if not (path:sub(1, 1) == "/" or path:sub(1, 1) == "~") then
+                        full_path = search_cwd .. "/" .. path
+                    end
+                    full_path = vim.fn.fnamemodify(full_path, ":p")
+
+                    vim.cmd("cd " .. vim.fn.fnameescape(full_path))
+                    vim.notify("Changed directory to: " .. full_path, vim.log.levels.INFO)
+                    if open_oil then
+                        require("oil").open(full_path)
+                    end
+                end
+            end)
+            return true
+        end
+    })
+end
+
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "_", "<CMD>Oil .<CR>", { desc = "Open Oil in CWD" })
+
+vim.keymap.set("n", "<leader>gd", function()
+    change_directory({
+        open_oil = false,
+        cwd = os.getenv("HOME"),
+        prompt_title = "📂 Global Change Directory (HOME)"
+    })
+end, { desc = "Change directory (HOME)" })
+
+vim.keymap.set("n", "<leader>cd", function()
+    change_directory({
+        open_oil = false,
+        cwd = vim.fn.getcwd(),
+        prompt_title = "📂 Local Change Directory (CWD)"
+    })
+end, { desc = "Change directory (CWD)" })
+
+vim.keymap.set("n", "<leader>co", function()
+    change_directory({
+        open_oil = true,
+        cwd = os.getenv("HOME"),
+        prompt_title = "📂 Change Dir & Open Oil"
+    })
+end, { desc = "Change dir and open Oil" })
