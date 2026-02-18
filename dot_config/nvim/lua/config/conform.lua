9bea0d4d6777d661deac75e3d05e91d002273e36
nkermani <kermani.nathan@gmail.com>
2026-02-18 02:03:42 +0100

    feat: add toggle for format on save and clang-format to CLI tools

--- dot_config/nvim/lua/config/conform.lua
+++ dot_config/nvim/lua/config/conform.lua
@@ -3,10 +3,22 @@
 
 require("conform").setup({
     formatters_by_ft = {
-        lua = {},  -- stylua disabled, will use LSP fallback
+        lua = {},
         python = { "isort", "black" },
         c = { "clang-format" },
         rust = { "rustfmt" },
     },
-    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+    end,
+    notify_on_error = false,
 })
+
+vim.keymap.set("n", "<leader>tf", function()
+    vim.g.disable_autoformat = not vim.g.disable_autoformat
+    local status = vim.g.disable_autoformat and "OFF" or "ON"
+    vim.notify("Format on Save " .. status, vim.log.levels.INFO, { title = "Conform" })
+end, { desc = "Toggle Format on Save" })
