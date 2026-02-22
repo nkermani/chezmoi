ba542bf4bc1dbe601f04c8ec822419aa1f140959
nkermani <kermani.nathan@gmail.com>
2026-02-22 18:35:35 +0100

    ```chore: remove deprecated configs and restructure nvim setup```

--- dot_config/nvim_full/lua/core/keymaps.lua
+++
@@ -1,8 +0,0 @@
--- lua/core/keymaps.lua
--- Entry point for all keymaps
-
-require("core.keymaps.navigation")
-require("core.keymaps.general")
-require("core.keymaps.editing")
-require("core.keymaps.selection")
-require("core.keymaps.modern")
--- dot_config/nvim_nvchad/init.lua
+++ dot_config/nvim_nvchad/init.lua
@@ -1,13 +1,37 @@
-local nvchad_repo = "https://github.com/NvChad/NvChad"
+vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
+vim.g.mapleader = " "
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/nvchad"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git", "clone", "--depth", "1", nvchad_repo, lazypath })
+-- bootstrap lazy and all plugins
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
 end
+
 vim.opt.rtp:prepend(lazypath)
 
-require("nvchad.bootstrap")
+local lazy_config = require "configs.lazy"
 
-vim.defer_fn(function()
-  require("nvchad.config"):load()
-end, 50)
+-- load plugins
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+  },
+
+  { import = "plugins" },
+}, lazy_config)
+
+-- load theme
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
