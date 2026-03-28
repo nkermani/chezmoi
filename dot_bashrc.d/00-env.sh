6ba0f0e13807ada40a7968392b040aea5bc42903
nkermani <kermani.nathan@gmail.com>
2026-03-28 12:20:42 +0100

    chores: setting up bash as main shell for faster prompts, more control, as well as vanilla editors

---
+++ dot_bashrc.d/00-env.sh
@@ -0,0 +1,15 @@
+# ~/.bashrc.d/00-env.sh - shared NKERMANI env for bash
+export NK_DIR="$HOME/.nkermani"
+export NK_APPS="$NK_DIR/apps"
+export NK_BIN="$NK_DIR/bin"
+export PATH="$NK_BIN:$HOME/.local/bin:$HOME/.opencode/bin:$HOME/.cargo/bin:$PATH"
+
+# OSX / Linux path helpers
+if [ "$(uname -s)" = "Darwin" ]; then
+    export PATH="/usr/local/bin:/opt/homebrew/bin:$PATH"
+else
+    export PATH="$HOME/.OpenJDK21U-jdk_x64_linux_hotspot_21.0.10_7 OpenJDK21U-jdk_x64_linux_hotspot_21.0.10_7/jdk-21.0.10+7/bin:$PATH"
+    export PATH="$HOME/.local/share/junest/bin:$PATH"
+fi
+
+export PATH
