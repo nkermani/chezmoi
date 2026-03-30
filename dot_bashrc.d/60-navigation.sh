42b2684155025d05ec407d6ec6e9b77016aef0d4
nkermani <kermani.nathan@gmail.com>
2026-03-30 16:37:38 +0200

    chore: update templates for better bashrc organization and consistency

--- dot_bashrc.d/60-navigation.sh
+++ dot_bashrc.d/60-navigation.sh
@@ -8,10 +8,18 @@ f() {
 	local dir
 	if [[ "$1" == "-a" ]]; then
 		dir=$(fd --type d --hidden --exclude .git --exclude .cache | fzf --prompt="📂 Go to (All): ")
+	elif [[ "$1" == "-t" ]]; then
+		dir=$(fd --type d --hidden --exclude .git --exclude .cache . | fzf --prompt="📂 Go to (Tree): " --height=40% --layout=reverse-list)
 	else
 		dir=$(fd --type d --exclude .git --exclude .cache | fzf --prompt="📂 Go to: ")
 	fi
 
+	[[ -n "$dir" ]] && cd "$dir"
+}
+
+ft() {
+	local dir
+	dir=$(fd --type d --hidden --exclude .git --exclude .cache . | fzf --prompt="📂 Tree: " --height=40% --reverse --preview 'if [ -d {} ]; then tree -L 2 {}; else bat --color=always {}; fi' --preview-window=right:40%)
 	[[ -n "$dir" ]] && cd "$dir"
 }
 
@@ -24,4 +32,28 @@ fr() {
 	fi
 
 	[[ -n "$dir" ]] && cd "$dir"
+}
+
+fe() {
+	local file
+	file=$(fd --type f --hidden --exclude .git --exclude .cache . | fzf --prompt="📄 Open: ")
+	[[ -n "$file" ]] && hx "$file"
+}
+
+fea() {
+	local file
+	file=$(fd --type f --hidden . | fzf --prompt="📄 Open (All): ")
+	[[ -n "$file" ]] && hx "$file"
+}
+
+fer() {
+	local file
+	file=$(fd --type f --hidden --exclude .git --exclude .cache . ~ | fzf --prompt="📄 Open Home: ")
+	[[ -n "$file" ]] && hx "$file"
+}
+
+fet() {
+	local file
+	file=$(fd --hidden --exclude .git --exclude .cache . | fzf --prompt="📄 Tree: " --height=40% --reverse --preview 'if [ -d {} ]; then tree -L 2 {}; else bat --color=always {}; fi' --preview-window=right:40%)
+	[[ -n "$file" ]] && hx "$file"
 }
