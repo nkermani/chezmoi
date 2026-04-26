52e1c4b1fbe0821dc27c105b94958c376aec9360
nkermani <kermani.nathan@gmail.com>
2026-04-26 16:40:27 +0200

    fix: kitty has absolute path, might need to check template in chezmoi in case of any path issues

--- dot_config/home-manager/modules/home.nix
+++ dot_config/home-manager/modules/home.nix
@@ -1,16 +1,35 @@
 { config, pkgs, ... }:
 
 {
-  home.username = builtins.getEnv "USER";
-  home.homeDirectory = builtins.getEnv "HOME";
+  home.username = "nkermani";
+  home.homeDirectory = "/home/nkermani";
+  home.stateVersion = "24.11";
 
-  home.sessionVariables = {
-    STUDENT_USERNAME = builtins.getEnv "STUDENT_USERNAME";
-    STUDENT_EMAIL = builtins.getEnv "STUDENT_EMAIL";
-    EDITOR="code";
-    VISUAL="code";
-    GIT_EDITOR="code";
+  xdg.configFile = {
+    "fontconfig/fonts.conf".text = ''
+      <?xml version="1.0" encoding="utf-8"?>
+      <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+      <fontconfig>
+        <dir prefix="cwd">/home/nkermani/.nix-profile/share/fonts</dir>
+        <dir prefix="cwd">/home/nkermani/.local/share/fonts</dir>
+      </fontconfig>
+    '';
+    "fish/config.fish".text = ''
+      # Load nix
+      if test -f ~/.nix-profile/etc/profile.d/nix-daemon.fish
+        source ~/.nix-profile/etc/profile.d/nix-daemon.fish
+      end
+
+      # Add Nix bin to PATH
+      if test -d ~/.nix-profile/bin
+        set -gx PATH ~/.nix-profile/bin $PATH
+      end
+    '';
   };
 
-  home.stateVersion = "25.11";
+  home.sessionVariables = {
+    PATH = "$HOME/.local/bin:$PATH";
+    STUDENT_USERNAME = "nkermani";
+    STUDENT_EMAIL = "nima.kermani@example.com";
+  };
 }
