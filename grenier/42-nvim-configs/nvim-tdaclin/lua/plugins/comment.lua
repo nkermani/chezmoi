0abe428f86c3da9c744cb6e717c556f3857d6d78
nkermani <kermani.nathan@gmail.com>
2026-05-08 00:09:50 +0200

    remove: removing grenier

--- grenier/42-nvim-configs/nvim-tdaclin/lua/plugins/comment.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"numToStr/Comment.nvim", version = "0.8.*",
-	opts = {
-		ignore = "^$",
-	}
-}
--- grenier/42-nvim-configs/nvim-tdaclin/lua/plugins/overseer.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"stevearc/overseer.nvim",
-	lazy = false,
-	keys = {
-		{ "<leader>ro", "<cmd>OverseerToggle<cr>" },
-		{ "<leader>rr", "<cmd>OverseerRun<cr>" },
-	},
-	opts = {},
-}
