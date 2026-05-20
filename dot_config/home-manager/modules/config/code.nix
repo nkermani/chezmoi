45ae4e4cce8a01e3d9b033aeecadcf4c5a04a337
nkermani <kermani.nathan@gmail.com>
2026-05-20 16:06:44 +0200

    feat: now code on Linux, will always get latest version of vscode

--- dot_config/home-manager/modules/config/code.nix
+++ dot_config/home-manager/modules/config/code.nix
@@ -1,8 +1,21 @@
 { config, pkgs, ... }:
 
+let
+  latestRelease = builtins.fromJSON (builtins.readFile (builtins.fetchurl {
+    url = "https://api.github.com/repos/microsoft/vscode/releases/latest";
+  }));
+  vscodeVersion = latestRelease.tag_name;
+in
 {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode;
+    package = pkgs.vscode.overrideAttrs (old: {
+      version = vscodeVersion;
+
+      src = builtins.fetchurl {
+        url = "https://update.code.visualstudio.com/${vscodeVersion}/linux-x64/stable";
+        name = "VSCode_${vscodeVersion}_linux-x64.tar.gz";
+      };
+    });
   };
 }
