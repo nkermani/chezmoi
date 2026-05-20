368b25f9b1bebdd9854ef274fb296dc8e617f7cd
nkermani <kermani.nathan@gmail.com>
2026-05-20 17:23:18 +0200

    test: adding neovide to have an alternative to vscode instead of zed alternative for vscode

---
+++ dot_config/home-manager/modules/config/neovide.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, ... }:
+
+let
+  latestRelease = builtins.fromJSON (builtins.readFile (builtins.fetchurl {
+    url = "https://api.github.com/repos/neovide/neovide/releases/latest";
+  }));
+  neovideVersion = latestRelease.tag_name;
+in
+{
+  home.packages = [
+    (pkgs.stdenv.mkDerivation {
+      pname = "neovide";
+      version = neovideVersion;
+
+      src = builtins.fetchurl {
+        url = "https://github.com/neovide/neovide/releases/download/${neovideVersion}/neovide-linux-x86_64.tar";
+        name = "neovide-${neovideVersion}-linux-x86_64.tar";
+      };
+
+      sourceRoot = ".";
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp neovide $out/bin/
+      '';
+    })
+  ];
+}
