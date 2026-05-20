0955b7fb33981c1b49d3a826b17ae0b3dd718cfb
nkermani <kermani.nathan@gmail.com>
2026-05-20 18:06:53 +0200

    test: added zed

---
+++ dot_config/home-manager/modules/config/zed.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, lib, ... }:
+
+let
+  latestRelease = builtins.fromJSON (builtins.readFile (builtins.fetchurl {
+    url = "https://api.github.com/repos/zed-industries/zed/releases/latest";
+  }));
+  zedVersion = latestRelease.tag_name;
+  arch = if pkgs.stdenv.hostPlatform.system == "aarch64-linux" then "aarch64" else "x86_64";
+
+  zedPackage = pkgs.stdenv.mkDerivation {
+    pname = "zed-editor";
+    version = builtins.substring 1 255 zedVersion;
+
+    src = builtins.fetchurl {
+      url = "https://github.com/zed-industries/zed/releases/download/${zedVersion}/zed-linux-${arch}.tar.gz";
+      name = "zed-${zedVersion}-linux-${arch}.tar.gz";
+    };
+
+    sourceRoot = "zed.app";
+
+    dontPatchELF = true;
+    dontStrip = true;
+
+    installPhase = ''
+      mkdir -p $out/bin $out/libexec $out/share
+
+      cp -r bin/* $out/bin/
+      cp -r libexec/* $out/libexec/
+      cp -r share/* $out/share/
+      cp -r lib $out/
+
+      ln -s $out/bin/zed $out/bin/zeditor
+    '';
+
+    meta = {
+      description = "High-performance, multiplayer code editor from the creators of Atom and Tree-sitter";
+      homepage = "https://zed.dev";
+      license = lib.licenses.gpl3Only;
+      mainProgram = "zeditor";
+      platforms = lib.platforms.linux;
+    };
+  };
+in
+{
+  programs.zed-editor = {
+    enable = true;
+    package = zedPackage;
+  };
+}
