33e427f77140871a8e9443dab0b61a662529840f
nkermani <kermani.nathan@gmail.com>
2026-05-23 00:25:30 +0200

    add: added few config in fresh.nix

--- dot_config/home-manager/modules/config/editors/fresh.nix
+++ dot_config/home-manager/modules/config/editors/fresh.nix
@@ -1,4 +1,12 @@
 {
   programs.fresh-editor = {
+    settings = {
+      editor = {
+        diagnostics_inline_text = true;
+      };
+      file_explorer = {
+        show_hidden = true;
+      };
+    };
   };
 }
