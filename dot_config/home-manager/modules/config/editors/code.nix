e12243503367ce418ea8c3d84fd030ebc9915d69
nkermani <kermani.nathan@gmail.com>
2026-05-31 00:48:55 +0200

    feat: added patchPhase

--- dot_config/home-manager/modules/config/editors/code.nix
+++ dot_config/home-manager/modules/config/editors/code.nix
@@ -11,11 +11,20 @@ in
   programs.vscode = {
     package = pkgs.vscode.overrideAttrs (old: {
       version = vscodeVersion;
-      
+
       src = builtins.fetchurl {
         url = "https://update.code.visualstudio.com/${vscodeVersion}/linux-x64/stable";
         name = "VSCode_${vscodeVersion}_linux-x64.tar.gz";
       };
+
+      # Ensure the ripgrep directory exists and handle missing binary in newer VS Code versions
+      postPatch = ''
+        mkdir -p resources/app/node_modules/@vscode/ripgrep/bin
+        rm -rf resources/app/node_modules/@github/copilot-linuxmusl-x64
+      '' + builtins.replaceStrings
+        ["rm resources/app/node_modules/@vscode/ripgrep/bin/rg"]
+        ["rm -f resources/app/node_modules/@vscode/ripgrep/bin/rg"]
+        old.postPatch;
     });
   };
 }
